28edf6125b190de6f137828ef947f6396f0537fa
Kye <kdavis6911>
2018-09-27 10:18:38 -0400

    Version 3

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GuessingGame
@@ -5,9 +6,14 @@ public class GuessingGame
     public static void main(String[] args) {
         Scanner choose = new Scanner(System.in);
         System.out.println("Please type the name of which game you want to play");
-        System.out.println("Type GuessingGame if you want to guess my number, and type ComputerGuess if you want me to guess your number!");
+        System.out.println("Type Guessinggame if you want to guess my number, and type Computerguess if you want me to guess your number!");
         String whichone = choose.nextLine();
-        if (whichone.equals("GuessingGame"))
+        while (!whichone.equals("Guessinggame") && !whichone.equals("Computerguess"))
+        {
+            System.out.println("I don't think I have that one on my system");
+            whichone = choose.nextLine();
+        }
+        if (whichone.equals("Guessinggame"))
         {
             int guessnum = 0;
             int answer = (int) (Math.random() * 100);
@@ -15,48 +21,51 @@ public class GuessingGame
             System.out.println("Please type in the number I am thinking of");
             int guess = input.nextInt();
             while (guess != answer) {
-                if (guess > answer) {
-                    System.out.println("It's too high! Try going lower");
-                    guess = input.nextInt();
-                    guessnum++;
+                try
+                {
+                    if (guess > answer) {
+                        System.out.println("It's too high! Try going lower");
+                        guess = input.nextInt();
+                        guessnum++;
+                    }
+                    if (guess < answer) {
+                        System.out.println("It's too low! Try going higher");
+                        guess = input.nextInt();
+                        guessnum++;
+                    }
+                    if (guess == answer) {
+                        guessnum++;
+                        String winstatement = "You did it! It took" + " " + guessnum + " " + "tries!";
+                        System.out.println(winstatement);
+                    }
                 }
-                if (guess < answer) {
-                    System.out.println("It's too low! Try going higher");
-                    guess = input.nextInt();
-                    guessnum++;
+                catch(InputMismatchException e)
+                {
+                    System.out.println("I'm sorry but that's not a number, please try again");
                 }
-            }
-            if (guess == answer) {
-                guessnum++;
-                String winstatement = "You did it! It took" + " " + guessnum + " " + "tries!";
-                System.out.println(winstatement);
             }
         }
-        if(whichone.equals("ComputerGuess"))
-        {
+        if(whichone.equals("Computerguess")) {
             Scanner amIright = new Scanner(System.in);
             System.out.println("Okay, I want you to think of a number between 1 and 100");
-            System.out.println("");
+            System.out.println("Please type 'higher' if you number is greater than my guess. Please type 'lower' if your number is less than my guess. Type correct if I'm on the spot!");
             int AIguess = 50;
             int highest = 100;
             int lowest = 0;
             String computermess = "Is your number " + AIguess + "?";
             System.out.println(computermess);
             String yoursay = amIright.nextLine();
-            while (!yoursay.equals("correct"))
-            {
-                if (yoursay.equals("higher"))
-                {
+            while (!yoursay.equals("correct")) {
+                if (yoursay.equals("higher")) {
                     lowest = AIguess;
-                    AIguess = AIguess + (highest - lowest)/2;
+                    AIguess = AIguess + (highest - lowest) / 2;
                     computermess = "Is your number " + AIguess + "?";
                     System.out.println(computermess);
                     yoursay = amIright.nextLine();
                 }
-                if (yoursay.equals("lower"))
-                {
+                if (yoursay.equals("lower")) {
                     highest = AIguess;
-                    AIguess = AIguess - (highest - lowest)/2;
+                    AIguess = AIguess - (highest - lowest) / 2;
                     computermess = "Is your number " + AIguess + "?";
                     System.out.println(computermess);
                     yoursay = amIright.nextLine();
